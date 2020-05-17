16f3b177f2679bde71b2bfcd37fff2cc798db4d9
Supaluck Singjan <supalucksingjan@gmail.com>
2020-05-18 04:12:10 +0700

    Added..

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 (() => {
     fetch('customers.json')
         .then( response => response.json())
-        .then( data => appendData(data))
+        .then( data => addCustomer(data))
         .catch(err => {console.log(err)});
 
-    function appendData(data) {
+    function addCustomer(data) {
         const myData = document.getElementById("data");
         for (let i = 0; i < data.length; i++) {
-            const div = document.createElement("li");
-            div.innerHTML = "Name : " + data[i].firstName 
-                + ' ' + data[i].lastName;
-            myData.appendChild(div);
+            const tr = document.createElement("tr");
+            tr.innerHTML =  "<td>" + data[i].id + "</td>"
+                + "<td>" +  data[i].firstName 
+                + ' ' + data[i].lastName + "</td>" 
+                + "<td> " + data[i].age + "</td>"
+                + "<td> " + data[i].country + "</td>";
+            myData.appendChild(tr);
 ;        }
     }
 })();
