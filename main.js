29cbbc2dfa8ee5a2b7a223b5b327bf280ce6cbd7
Supaluck Singjan <supalucksingjan@gmail.com>
2020-05-18 03:33:45 +0700

    Added main.js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log();
+(() => {
+    fetch('customers.json')
+        .then( response => response.json())
+        .then( data => appendData(data))
+        .catch(err => {console.log(err)});
+
+    function appendData(data) {
+        const myData = document.getElementById("data");
+        for (let i = 0; i < data.length; i++) {
+            const div = document.createElement("div");
+            div.innerHTML = "Name : " + data[i].firstName 
+                + ' ' + data[i].lastName;
+            myData.appendChild(div);
+;        }
+    }
+})();
